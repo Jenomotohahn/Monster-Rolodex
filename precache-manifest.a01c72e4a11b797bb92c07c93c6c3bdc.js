3b62b9a442af78fa10c93489146e05a617f878b4
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-12-04 14:39:32 -1000

    Updates

---
+++ precache-manifest.a01c72e4a11b797bb92c07c93c6c3bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49298476c366f4c39065bd92d20041d0",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "287e246a344df609f1fa",
+    "url": "/Monster-Rolodex/static/css/main.07453ecc.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/Monster-Rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "287e246a344df609f1fa",
+    "url": "/Monster-Rolodex/static/js/main.78c0ae0f.chunk.js"
+  },
+  {
+    "revision": "2ace4f71c22d2f0295e1",
+    "url": "/Monster-Rolodex/static/js/runtime-main.200a0233.js"
+  }
+]);
